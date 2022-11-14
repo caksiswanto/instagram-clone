155210be84f9baca294f23d32afa2e9db7453c1f
Agus Siswanto <cak_guess@ymail.com>
2022-11-14 09:35:19 +0700

    add search bar to the header component

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,40 @@
 import Image from 'next/image';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 const Header = () => {
 	return (
-		<div>
+		<div className='flex items-center justify-between max-w-6xl'>
 			{/* Left */}
-			<div className='flex items-center justify-between max-w-6xl'>
-				<div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
-					<Image
-						src='http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png'
-						fill
-						className='object-contain'
-					/>
-				</div>
-				<div className='cursor-pointer h-24 w-10 relative lg:hidden'>
-					<Image
-						src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png'
-						fill
-						className='object-contain'
-					/>
-				</div>
-
-				<h1>Right Side</h1>
+			<div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
+				<Image
+					src='http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png'
+					fill
+					className='object-contain'
+				/>
+			</div>
+			<div className='cursor-pointer h-24 w-10 relative lg:hidden'>
+				<Image
+					src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png'
+					fill
+					className='object-contain'
+				/>
 			</div>
 
 			{/* Middle */}
+			<div className='relative'>
+				<div className='absolute top-[50%] left-2 -translate-y-[50%]'>
+					<MagnifyingGlassIcon className='h-5 text-gray-500' />
+				</div>
+				<input
+					type='text'
+					placeholder='Search'
+					className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'
+				/>
+			</div>
 
 			{/* Right */}
+
+			<h1>Right Side</h1>
 		</div>
 	);
 };
