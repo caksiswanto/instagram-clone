c43b8c28dd00dfbbb4bb42f0f2fa7547709910a6
Agus Siswanto <cak_guess@ymail.com>
2023-01-10 17:00:45 +0700

    install recoil and add atom and UploadModal component

--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,12 @@ import Image from 'next/image';
 import { MagnifyingGlassIcon, PlusCircleIcon } from '@heroicons/react/24/outline';
 import { HomeIcon } from '@heroicons/react/24/solid';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import { useRecoilState } from 'recoil';
+import { modalState } from '../atom/modalAtom';
 
 const Header = () => {
 	const { data: session } = useSession();
-	console.log(session);
+	const [open, setOpen] = useRecoilState(modalState);
 
 	return (
 		<div className='shadow-sm border-b sticky top-0 bg-white z-30'>
@@ -44,7 +46,10 @@ const Header = () => {
 
 					{session ? (
 						<>
-							<PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
+							<PlusCircleIcon
+								onClick={() => setOpen(true)}
+								className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'
+							/>
 							<img
 								onClick={signOut}
 								src={session.user.image}
