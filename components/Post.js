6a08f1a5ba8cabf2e743ee8f4561a8db26b7d5cf
Agus Siswanto <cak_guess@ymail.com>
2023-01-11 22:50:04 +0700

    show comments in the comments section and get data from the firebase database

--- components/Post.js
+++ components/Post.js
@@ -5,14 +5,32 @@ import {
 	HeartIcon,
 } from '@heroicons/react/24/outline';
 import { EllipsisHorizontalIcon } from '@heroicons/react/24/solid';
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import {
+	addDoc,
+	collection,
+	onSnapshot,
+	orderBy,
+	query,
+	serverTimestamp,
+} from 'firebase/firestore';
 import { useSession } from 'next-auth/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Moment from 'react-moment';
 import { db } from '../firebase';
 
 export default function Post({ id, username, userImg, img, caption }) {
 	const { data: session } = useSession();
 	const [comment, setComment] = useState('');
+	const [comments, setComments] = useState([]);
+
+	useEffect(() => {
+		const unsubscribe = onSnapshot(
+			query(collection(db, 'posts', id, 'comments'), orderBy('timestamp', 'desc')),
+			(snapshot) => {
+				setComments(snapshot.docs);
+			}
+		);
+	}, [db, id]);
 
 	const sendComment = async (event) => {
 		event.preventDefault();
@@ -61,6 +79,23 @@ export default function Post({ id, username, userImg, img, caption }) {
 				<span className='font-bold mr-2'>{username}</span>
 				{caption}
 			</p>
+
+			{comments.length > 0 && (
+				<div className='mx-10 max-h-24 overflow-y-scroll scrollbar-none'>
+					{comments.map((comment) => (
+						<div className='flex items-center space-x-2 mb-2'>
+							<img
+								className='h-7 rounded-full object-cover'
+								src={comment.data().userImage}
+								alt='user-image'
+							/>
+							<p className='font-semibold'>{comment.data().username}</p>
+							<p className='flex-1 truncate'>{comment.data().comment}</p>
+							<Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
+						</div>
+					))}
+				</div>
+			)}
 
 			{/* Post Input Box */}
 			{session && (
