50be213ef1b7d6c14aa8ad8a44a606e78a7e118c
Agus Siswanto <cak_guess@ymail.com>
2023-01-11 23:56:59 +0700

    apply like functionality

--- components/Post.js
+++ components/Post.js
@@ -4,14 +4,18 @@ import {
 	FaceSmileIcon,
 	HeartIcon,
 } from '@heroicons/react/24/outline';
+import { HeartIcon as HeartIconFilled } from '@heroicons/react/24/solid';
 import { EllipsisHorizontalIcon } from '@heroicons/react/24/solid';
 import {
 	addDoc,
 	collection,
+	deleteDoc,
+	doc,
 	onSnapshot,
 	orderBy,
 	query,
 	serverTimestamp,
+	setDoc,
 } from 'firebase/firestore';
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
@@ -22,6 +26,8 @@ export default function Post({ id, username, userImg, img, caption }) {
 	const { data: session } = useSession();
 	const [comment, setComment] = useState('');
 	const [comments, setComments] = useState([]);
+	const [likes, setLikes] = useState([]);
+	const [hasLiked, setHasLiked] = useState(false);
 
 	useEffect(() => {
 		const unsubscribe = onSnapshot(
@@ -31,6 +37,26 @@ export default function Post({ id, username, userImg, img, caption }) {
 			}
 		);
 	}, [db, id]);
+
+	useEffect(() => {
+		const unsubscribe = onSnapshot(collection(db, 'posts', id, 'likes'), (snapshot) =>
+			setLikes(snapshot.docs)
+		);
+	}, [db]);
+
+	useEffect(() => {
+		setHasLiked(likes.findIndex((like) => like.id === session?.user.uid) !== -1);
+	}, [likes]);
+
+	const likePost = async () => {
+		if (hasLiked) {
+			await deleteDoc(doc(db, 'posts', id, 'likes', session.user.uid));
+		} else {
+			await setDoc(doc(db, 'posts', id, 'likes', session.user.uid), {
+				username: session.user.username,
+			});
+		}
+	};
 
 	const sendComment = async (event) => {
 		event.preventDefault();
@@ -66,7 +92,11 @@ export default function Post({ id, username, userImg, img, caption }) {
 			{session && (
 				<div className='flex justify-between px-4 pt-4'>
 					<div className='flex space-x-4'>
-						<HeartIcon className='btn' />
+						{hasLiked ? (
+							<HeartIconFilled onClick={likePost} className='btn text-red-400' />
+						) : (
+							<HeartIcon onClick={likePost} className='btn' />
+						)}
 						<ChatBubbleOvalLeftIcon className='btn' />
 					</div>
 
